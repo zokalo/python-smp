5fae7a9d498220c75b0df61fb054f7e75a0ee786
Dmitry Bashkatov <dbashkatov@gmail.com>
2017-09-09 04:21:44 +0300

    improve client

--- src/smp/client.py
+++ src/smp/client.py
@@ -2,6 +2,12 @@ from utils.apiclient import BaseApiClient, ApiError
 from utils.apiclient.mixins import HelperMethodsMixin
 
 
+class Response(dict):
+    def __init__(self, response):
+        super().__init__(response.json())
+        self.response = response
+
+
 class SmpApiClient(HelperMethodsMixin, BaseApiClient):
     base_url = 'https://api.smp.io/'
 
@@ -12,4 +18,4 @@ class SmpApiClient(HelperMethodsMixin, BaseApiClient):
             if response.headers.get('content-type') == 'application/json':
                 err.data = response.json()  # TODO: surround with try..except
             raise err
-        return response.json()
+        return Response(response)
