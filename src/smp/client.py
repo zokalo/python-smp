f811fd02d57cb5593363c3cfc27ff447720ec46f
Dmitry Bashkatov <dbashkatov@gmail.com>
2017-09-11 20:25:39 +0300

    update api client

--- src/smp/client.py
+++ src/smp/client.py
@@ -1,5 +1,11 @@
-from utils.apiclient import BaseApiClient, ApiError
+from utils.apiclient import BaseApiClient, ApiError, ApiRequest
 from utils.apiclient.mixins import HelperMethodsMixin
+
+
+class Request(ApiRequest):
+    def __init__(self, *args, **kwargs):
+        self.json_response = kwargs.pop('json_response', True)
+        super().__init__(*args, **kwargs)
 
 
 class Response(dict):
@@ -8,14 +14,23 @@ class Response(dict):
         self.response = response
 
 
-class SmpApiClient(HelperMethodsMixin, BaseApiClient):
+class SmpApiClientMetaClass(type(HelperMethodsMixin), type(BaseApiClient)):
+    pass
+
+
+class SmpApiClient(HelperMethodsMixin, BaseApiClient, metaclass=SmpApiClientMetaClass):
     base_url = 'https://api.smp.io/'
+    request_class = Request
 
     def clean_response(self, response, request):
         try:
-            super(SmpApiClient, self).clean_response(response, request)
+            content = super(SmpApiClient, self).clean_response(response, request)
         except ApiError as err:
             if response.headers.get('content-type') == 'application/json':
                 err.data = response.json()  # TODO: surround with try..except
             raise err
-        return Response(response)
+
+        if request.json_response:
+            return Response(response)
+        else:
+            return content
