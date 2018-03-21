9b0d4770e6f02481fa3d810de5d7b0d4ca1809ee
Dmitry Bashkatov <dbashkatov@gmail.com>
2018-03-21 19:35:41 +0300

    explicit auth user handling

--- src/smp/auth.py
+++ src/smp/auth.py
@@ -1,15 +1,22 @@
 import datetime
 
 
+undefined_user = type('undefined_user', (), {})()
+
+
 def create_jwt(app_id, app_secret, user_id=None, scopes=None, duration=None):
     import jwt
 
     if duration is None:
         duration = datetime.timedelta(minutes=5)
 
-    return jwt.encode({
+    payload = {
         'iss': app_id,
-        'sub': user_id,
         'scp': scopes,
         'exp': datetime.datetime.utcnow() + duration,
-    }, app_secret, algorithm='HS256')
+    }
+
+    if user_id is not undefined_user:
+        payload['sub'] = user_id
+
+    return jwt.encode(payload, app_secret, algorithm='HS256')
