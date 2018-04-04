2270fe04d32d4192271c9b0d9efa8ff3008fa97b
Dmitry Bashkatov <dbashkatov@gmail.com>
2018-04-04 15:25:51 +0300

    remove useless models

--- src/smp/__init__.py
+++ src/smp/__init__.py
@@ -1,3 +1,2 @@
 from . import auth  # noqa
-from . import models  # noqa
 from .client import SmpApiClient  # noqa
--- src/smp/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# flake8: noqa: F401
-
-from .AccountCredentialPermission import AccountCredentialPermission
-from .AccountCredentialType import AccountCredentialType
-from .CredentialExchangeType import CredentialExchangeType
-from .Medium import Medium
