633c9b94afc9ae02a1e093bc9c0b37dc781fbcc3
Dmitriy Don <dmitriysdon@gmail.com>
2020-07-20 14:25:44 +0300

    restore empty-in-hack

--- src/smp/client.py
+++ src/smp/client.py
@@ -11,17 +11,19 @@ from httpapiclient.mixins import JsonResponseMixin, HelperMethodsMixin
 
 class SmpApiRequest(ApiRequest):
     def __init__(self, *args, **kwargs):
+        self.empty = False
         kwargs = self._prepare_params(kwargs)
         super().__init__(*args, **kwargs)
 
-    @staticmethod
-    def _prepare_params(kwargs):
+    def _prepare_params(self, kwargs):
         """
         django-filters lookup type "__in" in most cases support only ?field__in=v1,v2,v3 syntax
         also it makes your query params shorter
         """
         params = kwargs.get('params', {})
         for param, value in params.items():
+            if param.endswith('__in') and not value:
+                self.empty = True
             if isinstance(value, (set, list, tuple)):
                 params[param] = ','.join(str(i) for i in value)
         return kwargs
@@ -142,6 +144,18 @@ class SmpApiClient(JsonResponseMixin, HelperMethodsMixin, BaseApiClient):
                     return
                 yield row
                 resource_counter += 1
+
+    def _request_once(self, request, prepeared, timeout):
+        if request.empty:
+            if request.raw_response:
+                raise NotImplementedError('raw response for empty response')
+            else:
+                return {
+                    'next': None,
+                    'previous': None,
+                    'results': [],
+                }
+        return super()._request_once(request, prepeared, timeout)
 
 
 class MediaClient(SmpApiClient):
